2369c5c7a1320b4e9ff69a43e8128e770df41b57
Emilie <chouchouneed@hotmail.fr>
2018-01-25 10:28:21 +0100

    Creation de la structure de l'arbre

--- projet/headers/struct.h
+++ projet/headers/struct.h
@@ -3,5 +3,30 @@
 #include <stdbool.h>
 
 typedef struct Node {
-
+  unsigned int key;
+  struct node *left;
+  struct node *right;
 } Node;
+
+/* @function : addNode
+ * @description: Permet d'ajouter un noeud dans l'arbre
+ * @param : node **tree
+ * @param : unsigned int key
+  * @return : nothing
+*/
+void addNode(node **tree, unsigned int key);
+
+/* @function : searchNode
+ * @description: Permet de chercher un noeud spécifique enregistré dans l'arbre
+ * @param : node **tree
+ * @param : unsigned int key
+ * @return : int
+*/
+int searchNode(node *tree, unsigned int key);
+
+/* @function : clearTree
+ * @description: Permet de supprimer tous les noeuds de l'arbre
+ * @param : node **tree
+ * @return : nothing
+*/
+void clearTree(node **tree);
---
+++ projet/src/struct.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct Node {
+  unsigned int key;
+  struct node *left;
+  struct node *right;
+} Node;
+
+void addNode(node **tree, unsigned int key)
+{
+    node *tmpNode;
+    node *tmpTree = *tree;
+
+    node *elem = malloc(sizeof(node));
+    elem->key = key;
+    elem->left = NULL;
+    elem->right = NULL;
+
+    if(tmpTree)
+    do
+    {
+        tmpNode = tmpTree;
+        if(key > tmpTree->key )
+        {
+            tmpTree = tmpTree->right;
+            if(!tmpTree) tmpNode->right = elem;
+        }
+        else
+        {
+            tmpTree = tmpTree->left;
+            if(!tmpTree) tmpNode->left = elem;
+        }
+    }
+    while(tmpTree);
+    else  *tree = elem;
+}
+
+int searchNode(node *tree, unsigned int key)
+{
+    while(tree)
+    {
+        if(key == tree->key) return 1;
+
+        if(key > tree->key ) tree = tree->right;
+        else tree = tree->left;
+    }
+    return 0;
+}
+
+void clearTree(node **tree)
+{
+    node *tmpTree = *tree;
+    if(!tree) return;
+    if(tmpTree->left)  clearTree(&tmpTree->left);
+    if(tmpTree->right) clearTree(&tmpTree->right);
+
+    free(tmpTree);      
+    *tree = NULL;
+}

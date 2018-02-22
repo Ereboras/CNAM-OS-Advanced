d9e3e789724d7f233085be827a0e5fdd8cf181d3
Mohamed Azzouz <azzouzminfo@yahoo.fr>
2018-02-22 11:36:52 +0100

    Docs

--- projet/headers/linked_list.h
+++ projet/headers/linked_list.h
@@ -3,25 +3,31 @@
 
 #include "../headers/typedef.h"
 
-/* @function : createRoot
- * @description: Create a node with command
- * @param : char* cmd
- * @return : node*
-*/
+/**
+ * \fn createNode(char* cmd)
+ * \brief Cette fonction permet de créer un noeud à partir d'un string correspondant à une commande.
+ * 
+ * \param cmd est un string correspondant à un morceau de la commande lancé par l'utilisateur qui a été parsée
+ * \return node
+ */
 node* createNode(char* cmd);
 
-/* @function : createEmptyNode
- * @description: Create new empty nodes
- * @return : node*
-*/
+/**
+ * \fn createEmptyNode()
+ * \brief Cette fonction permet de créer un noeud vide.
+ * \return node
+ */
 node* createEmptyNode();
 
-/* @function : createNodeAndLinkNext
- * @description: Create node and link root with the new
- * @param : node *root
- * @param : node *cmd
- * @return : node*
-*/
+/**
+ * \fn createNodeAndLinkNext(node* root, char* cmd)
+ * \brief Cette fonction permet de créer un noeud à partir d'un string correspondant à une commande ainsi que du noeud qui le précède.
+ * La Fonction renvoie vers le noeud nouvellement créé.
+ * 
+ * \param root est le noeud dans lequel on souhaite enregistré le prochain noeud
+ * \param cmd est un string correspondant à un morceau de la commande lancé par l'utilisateur qui a été parsée
+ * \return node
+ */
 node* createNodeAndLinkNext(node* root, char* cmd);
 
 #endif
--- projet/src/linked_list.c
+++ projet/src/linked_list.c
@@ -7,39 +7,21 @@
 #include <stdio.h>
 #include "../headers/linked_list.h"
 
-/**
- * \fn createNode(char* cmd)
- * \brief Cette fonction permet de créer un noeud à partir d'un string correspondant à une commande.
- * 
- * \param cmd est un string correspondant à un morceau de la commande lancé par l'utilisateur qui a été parsée
- * \return node
- */
+
 node* createNode(char* cmd) {
     node *root = malloc(sizeof(node));
     root->command = cmd;
     return root;
 }
 
-/**
- * \fn createEmptyNode()
- * \brief Cette fonction permet de créer un noeud vide.
- * \return node
- */
+
 node* createEmptyNode() {
     node *root = malloc(sizeof(node));
     root->command = "";
     return root;
 }
 
-/**
- * \fn createNodeAndLinkNext(node* root, char* cmd)
- * \brief Cette fonction permet de créer un noeud à partir d'un string correspondant à une commande ainsi que du noeud qui le précède.
- * La Fonction renvoie vers le noeud nouvellement créé.
- * 
- * \param root est le noeud dans lequel on souhaite enregistré le prochain noeud
- * \param cmd est un string correspondant à un morceau de la commande lancé par l'utilisateur qui a été parsée
- * \return node
- */
+
 node* createNodeAndLinkNext(node* root, char* cmd) {
     node *temp = createNode(cmd);
     root->next = temp;

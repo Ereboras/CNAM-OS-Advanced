929b2ae08c92333be0d0b53870c7ed1db132ca9b
Mohamed Azzouz <azzouzminfo@yahoo.fr>
2018-04-16 11:05:39 +0200

    Add utils files

--- projet/headers/main.h
+++ projet/headers/main.h
@@ -2,47 +2,9 @@
 #define FILE_MAIN_HEADER
 
 #include "../headers/typedef.h"
+#include "../headers/utils.h"
 #include "../headers/commands.h"
 
-
-
-/**
- * \fn inputString(FILE* fp, size_t size)
- * \brief Dynamically allocate memory for user inputs
- * 
- * \param fp File descriptor of the input of the user
- * \param size Initial size of the user input, dynamically resized
- * \return char*
- */
-char* inputString(FILE* fp, size_t size);
-
-/**
- * \fn copyFile(FILE* filesrc, FILE* filedest)
- * \brief Copy content from source file and write it to destination file
- * 
- * \param filesrc Source file
- * \param filedest Destination file
- * \return int
- */
-int copyFile(FILE* filesrc, FILE* filedest);
-
-/**
- * \fn isOperator(char* operator)
- * \brief Check if string is a linux operator
- * 
- * \param operator String to check
- * \return bool
- */
-bool isOperator(char* operator);
-
-/**
- * \fn isBuiltInCommand(char* cmd)
- * \brief Check if string is a built in command
- * 
- * \param operator String to check
- * \return bool
- */
-bool isBuiltInCommand(char* cmd);
 
 /**
  * \fn logAction(char* command)
